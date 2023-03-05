febfca7f3e594e2a114176556a1baf02780411fa
OlegGalepa <113933558+OlegGalepa@users.noreply.github.com>
2023-03-06 00:09:58 +0200

    add-hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+console.log(galleryBox);
+
+function createGalleryMarkup(item) {
+  return item.map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;})
+    .join('');
+}
+
+galleryBox.innerHTML = createGalleryMarkup(galleryItems);
+
+galleryBox.addEventListener('click', onGalleryItemsClick);
+
+function onGalleryItemsClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.hasAttribute('data-source')) {
+    return;
+  }  
+
+  const instance = basicLightbox.create(
+    `<img  src="${evt.target.getAttribute('data-source')}" alt="${evt.target.getAttribute('alt')}">`,
+  {
+      onShow: (instance) => {
+        window.removeEventListener('keydown', onEscClick);
+      }
+    });
+
+  function onEscClick(evt) {
+    if (evt.key === 'Escape') {
+   instance.close()
+  }
+}
+  instance.show();
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector('.gallery');
+
+function createGalleryMarkup(item, parentEl) {
+  const GalleryMarkup = item.map(({preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;})
+    .join('');
+    
+    return (parentEl.innerHTML = GalleryMarkup);
+}    
+
+createGalleryMarkup(galleryItems, galleryBox);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+})
